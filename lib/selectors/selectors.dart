0227bc7d4a4d41452784ddf32d9165678ed4d3ee
Arne Olai Molland <arneolaim@gmail.com>
2018-11-24 18:00:24 +0100

    :art: Added an article selector

--- lib/selectors/selectors.dart
+++ lib/selectors/selectors.dart
@@ -1,7 +1,16 @@
 import 'package:kaffe_app/models/models.dart';
+import 'package:kaffe_app/optional.dart';
 
 List<Article> articlesSelector(AppState state) => state.articles;
 
 bool isLoadingSelector(AppState state) => state.isLoading;
 
 AppTab activeTabSelector(AppState state) => state.activeTab;
+
+Optional<Article> articleSelector(List<Article> articles, String id) {
+  try {
+    return Optional.of(articles.firstWhere((article) => article.id == id));
+  } catch (e) {
+    return Optional.absent();
+  }
+}
