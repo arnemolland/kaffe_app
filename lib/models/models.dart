2d4bdbe69ed05252ad2b36b7aaa436f2c4602e2c
Arne Olai Molland <arneolaim@gmail.com>
2018-11-25 16:58:54 +0100

    :construction: Exporting coffee models

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -2,4 +2,6 @@ export 'app_state.dart';
 export 'coffee.dart';
 export 'article.dart';
 export 'app_tab.dart';
+export 'coffee_type.dart';
+export 'coffee_bean.dart';
 export 'visibility_filter.dart';
